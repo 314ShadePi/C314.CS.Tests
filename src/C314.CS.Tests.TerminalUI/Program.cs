37029b3ba840e8dc639f7bf60339e6e43db8ebd0
314ShadePi <erikkirkegaardstage@gmail.com>
2022-05-05 14:51:28 +0200

    Add solution C314.CS.Tests and project C314.CS.Tests.TerminalUI

---
+++ src/C314.CS.Tests.TerminalUI/Program.cs
@@ -0,0 +1,12 @@
+namespace C314.CS.Tests.TerminalUI
+{
+	class Program
+	{
+		public static int Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			Console.ReadKey();
+			return 0;
+		}
+	}
+}
